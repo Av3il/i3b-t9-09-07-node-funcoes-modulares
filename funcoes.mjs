9cde602bdf873a41a827065e7b0d5a3ae718ca47
Alfredo <weltontimjose@gmail.com>
2025-11-26 11:18:24 -0300

    Primeiro commit

---
+++ funcoes.mjs
@@ -0,0 +1,16 @@
+export function funcao1() {
+    const n = 100;
+    console.log(2*n); 
+}
+
+export const funcao2 = function(){
+    const name = 'Juventus'
+    console.log(name);
+}
+
+export const funcao3 = () => {
+    const v = [2, 3, 6, 8]
+    console.log(v);
+}
+
+export const soma = (a, b) => a + b;
